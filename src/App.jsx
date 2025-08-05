23fe55ee4a91e4fa49f494d8294dc4cbade754a1
rafacosta87 <costarafa87@outlook.com>
2025-08-05 17:34:38 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div 
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center", 
+        justifyContent: "center",
+        border: "solid 2px red",
+        height: "100vh"
+      }}>
+        <header style={{ backgroundColor: "blue", color: 'white', fontSize: "40px" }}>Cabeçalho</header>
+        <div >Hello</div>
+      </div>
+    </>
+  )
+}
+
+export default App
